78160cc63cad7785bbdf9dfadd90e4d7812e3bcb
mahmoodjan8088 <mehmoodjan8088@gmail.com>
2026-02-13 15:50:47 +0500

    addition of values

---
+++ for-addittion-of-values.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    cout << "enter value of a" << endl;
+    cin >> a;
+    cout << "enter value of b" << endl;
+    cin >> b;
+    sum = a + b;
+    cout << "sum=" << sum << endl;
+
+    return 0;
+}
